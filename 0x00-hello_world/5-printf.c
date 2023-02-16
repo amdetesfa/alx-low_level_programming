3c4ea2c2946aa1855503ae3d5a6aebf00dea8ffb
amdetesfa <amdetesfa@gmail.com>
2023-02-16 15:33:23 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - A demo file for printf function call
- * Description: 'the program's description'
- * return: Always 0 (success)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
