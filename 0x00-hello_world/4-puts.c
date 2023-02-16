37f624ecd1881f0008d067720805ef071de37f4f
amdetesfa <amdetesfa@gmail.com>
2023-02-16 13:42:00 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - C program to print put function
+ * return: 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
