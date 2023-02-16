13565d5bc37e7fd7e5915783167313151d817f81
amdetesfa <amdetesfa@gmail.com>
2023-02-16 14:12:22 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A demo file for printf function call
+ * return: 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
