b41b008e75099799668cc8460e137ad332ca73ae
amdetesfa <amdetesfa@gmail.com>
2023-02-20 05:55:16 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: print digits
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
+
